f3790fedd6300aa62546a0ae9e6b244654920fb6
Arno <arno.deceuninck@student.uantwerpen.be>
2020-08-02 10:40:17 +0200

    Using temp files and passing in and output filenames

--- pdf-to-ereader.sh
+++ pdf-to-ereader.sh
@@ -1,3 +1,8 @@
-pdfcrop --margins '5 5 5 5' input.pdf output.pdf # Remove the margins
-mutool poster -x 2 input.pdf output.pdf # Split the pages in two
-pdftk input.pdf cat 1-endeast output output.pdf # Rotate the pdf 90 degrees
+temp1=$(mktemp)
+# For some reason mutool won't work with a second temp file as output
+pdfcrop --margins '5 5 5 5' $1 ${temp1} # Remove the margins
+mutool poster -y 2 ${temp1} out.pdf # Split the pages in two
+pdftk out.pdf cat 1-endeast output $2 # Rotate the pdf 90 degrees
+rm out.pdf
+rm ${temp1}
+
